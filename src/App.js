7c25bac003fcf906454b2bdea89d244c6c3afa71
Lamberto <asghedom.lamberto@gmail.com>
2020-03-16 15:35:27 -0700

    fetched data from HACKER API

--- src/App.js
+++ src/App.js
@@ -1,41 +1,35 @@
 import React, { useState, useReducer, useEffect, useRef } from 'react';
-
-const initialStories = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0
-  },
-  {
-    title: 'Redux',
-    url: 'https://reduxjs.org',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1
-  },
-];
-
-const getAsyncStories = () => 
-  new Promise(resolve => 
-    setTimeout(
-      () => resolve({ data: { stories: initialStories } }),
-      2000
-    )
-  );
  
 const storiesReducer = (state, action) => {
+  console.log('%c TYPE OF ACTION', 'color: white; background: black');
+  console.log(action.type);
   switch (action.type) {
-    case 'SET_STORIES':
-      return action.payload
-      break;
-    case 'REMOVE_STORY':
-      return state.filter(
-        story => action.payload.objectID !== story.objectID
-      );
+    case 'STORIES_FETCH_INIT':
+      return {
+        ...state,
+        isLoading: true,
+        isError: false
+      };
+    case 'STORIES_FETCH_SUCCESS':
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload
+      };
+    case 'STORIES_FETCH_FAILURE':
+      return {
+        ...state,
+        isLoading: false,
+        isError: true
+      };
+      case 'REMOVE_STORY':
+        return {
+          ...state,
+          data: state.data.filter(
+            story => action.payload.objectID !== story.objectID
+          ),
+        };
     default:
       throw new Error();
   }
@@ -59,23 +53,29 @@ const useSemiPersistentState = (key, initialState) => {
   return [value, setValue];
 };
 
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query='
 const App = () => {
-  const [stories, dispatchStories] = useReducer(storiesReducer, []);
+  const [stories, dispatchStories] = useReducer(
+    storiesReducer, 
+    { data: [], isLoading: false, isError: false }
+  );
+
   const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React');
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
-    setIsLoading(true);
-    getAsyncStories()
+    dispatchStories({ type: 'STORIES_FETCH_INIT' });
+
+    fetch(`${API_ENDPOINT}react`)
+      .then(response => response.json())
       .then(result => {
         dispatchStories({
-          type: 'SET_STORIES',
-          payload: result.data.stories
+          type: 'STORIES_FETCH_SUCCESS',
+          payload: result.hits
         });
-      setIsLoading(false);
-    })
-    .catch(() => setIsError(true));
+      })
+      .catch(() => 
+        dispatchStories({ type: 'STORIES_FETCH_FAILURE'})
+      )
   }, []);
 
   const handleRemoveStory = item => {
@@ -90,7 +90,7 @@ const App = () => {
     setSearchTerm(event.target.value);
   };
 
-  const searchedStories = stories.filter(story => 
+  const searchedStories = stories.data.filter(story => 
     story.title.toLowerCase().includes(searchTerm.toLowerCase())
   );
   return (
@@ -105,8 +105,8 @@ const App = () => {
         <strong>Search</strong>
       </InputWithLabel>
       <hr />
-      {isError && <p>Something went wrong ...</p>}
-      {isLoading ? (
+      {stories.isError && <p>Something went wrong ...</p>}
+      {stories.isLoading ? (
         <p>Is Loading ...</p>
       ) : (
         <List list={searchedStories} onRemoveItem={handleRemoveStory} />
