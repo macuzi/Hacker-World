259b7fa8d3c59e82efa32dc1f292824295aca7e7
Lamberto <asghedom.lamberto@gmail.com>
2020-03-25 16:26:18 -0700

    file structure clean up

---
+++ src/App.js
@@ -0,0 +1,109 @@
+import React, { useState, useReducer, useEffect, useCallback } from 'react';
+import axios from 'axios';
+
+import SearchForm from './SearchForm';
+import List from './List';
+
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
+// custom hook for local storage
+const useSemiPersistentState = (key, initialState) => {
+  const [value, setValue] = useState(localStorage.getItem(key) || initialState);
+
+  useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [value, key]);
+  return [value, setValue]
+}
+
+// reducers
+const storiesReducer = (state, action) => {
+  switch (action.type) {
+    case 'STORIES_FETCH_INIT':
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+    case 'STORIES_FETCH_SUCCESS':
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload
+      };
+    case 'STORIES_FETCH_FAILURE':
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
+    case 'REMOVE_STORY': 
+      return {
+        ...state,
+        data: state.data.filter(
+          story => action.payload.objectID !== story.objectID
+        ),
+      };
+    default:
+      throw new Error();
+  }
+}
+
+const App = () => {
+  const [stories, dispatchStories] = useReducer(storiesReducer, { data: [], isLoading: false, isError: false });
+  const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React');
+  const [url, setUrl] = useState(`${API_ENDPOINT}${searchTerm}`);
+
+  const handleFetchStories = useCallback(async () => {
+    dispatchStories({ type: 'STORIES_FETCH_INIT' });
+
+    try {
+      const result = await axios.get(url);
+
+      dispatchStories({
+        type: 'STORIES_FETCH_SUCCESS',
+        payload: result.data.hits
+      });
+    } catch {
+      dispatchStories({ type: 'STORIES_FETCH_FAILURE' });
+    }
+  }, [url]);
+
+  useEffect(() => {
+    handleFetchStories();
+  }, [handleFetchStories])
+
+  const handleRemoveStory = item => {
+    dispatchStories({ 
+      type: 'REMOVE_STORY', 
+      payload: item
+    });
+  };
+
+  const handleSearchInput = event => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSearchSubmit = event => {
+    setUrl(`${API_ENDPOINT}${searchTerm}`)
+    event.preventDefault();
+  };
+  return (
+    <>
+      <h1>Hacker Stories</h1>
+      <SearchForm 
+        searchTerm={searchTerm}
+        onSearchInput={handleSearchInput}
+        onSearchSubmit={handleSearchSubmit}
+      />
+      <hr />
+      {stories.isError && <p>Something Went wrong...</p>}
+      {stories.isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <List list={stories.data} onRemoveItem={handleRemoveStory} />
+      )}
+    </>
+  );
+}
+export default App;
---
+++ src/SearchForm.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import InputWithLabel from './InputWIthLabel';
+
+const SearchForm = ({ 
+  searchTerm, 
+  onSearchInput, 
+  onSearchSubmit 
+}) => (
+  <form onSubmit={onSearchSubmit}>
+    <InputWithLabel 
+      id="search"
+      value={searchTerm}
+      isFocused
+      onInputChange={onSearchInput}
+    >
+      <strong>Search:</strong>
+    </InputWithLabel>
+    <button type="submit" disabled={!searchTerm}>Submit</button>
+  </form>
+);
+
+export default SearchForm;
